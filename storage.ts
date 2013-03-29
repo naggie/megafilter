eff06bb00ceea49509161c186a12aa710ca5d443
Callan Bryant <callan.bryant@gmail.com>
2013-03-29 09:26:09 +0000

    more interface

--- storage.ts
+++ storage.ts
@@ -1,5 +1,5 @@
 // interfaces written in typescript which is too much of a pain to use with nodejs at the moment
-// to compile: /usr/local/share/npm/bin/tsc --module node storage.ts
+// to compile: /usr/local/share/npm/bin/tsc --comments --module node storage.ts
 // do this and use storage.js as a module
 // the inheritance is nice
 export interface Article {
@@ -43,10 +43,23 @@ export interface ArticleQueue {
 	// number of articles that have been published
 	published:number;
 
+	// move an article back to the queue
+	unpublish?(id:number) : void;
+
+	// keep the last discarded item to restore it to the queue
+	// may fail, if another client deleted in the mean time
+	//undiscard?(id:number) : bool;
+	// or just check trash
+
+	// current (last given) article index
+	index:number;
+
+	// last deleted article
+	trash:Article;
 }
 
 
-export class jsonQueue implements ArticleQueue {
+export class jsonArticleQueue implements ArticleQueue {
 	// add an Article to the queue
 	enqueue(article : Article) : void{}
 
