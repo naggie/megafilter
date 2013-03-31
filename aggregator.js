46942b2970f8cb2cc09892a3f558bfcea3e39ea2
Callan Bryant <callan.bryant@gmail.com>
2013-03-31 14:24:41 +0100

    implement low-level API methods

--- aggregator.js
+++ aggregator.js
@@ -3,4 +3,29 @@ config = require('./config')
 queue  = require('./articleQueue')[config.queue]
 store  = require('./articleStorage')[config.store]
 
-exports.
+
+// TODO: return pending count somehow (maybe attach to article)
+
+// TODO: 404
+exports.current = queue.current
+exports.next    = queue.next
+exports.dump    = store.dump
+
+exports.publish = function() {
+
+}
+
+// TODO: 404
+exports.discard = function(id) {
+	return !!queue.extract(id)
+}
+
+
+// TODO: 404
+exports.publish = function(id) {
+	var article = queue.extract(id)
+	if (article)
+		return !!store.save(article)
+	else
+		return false
+}
