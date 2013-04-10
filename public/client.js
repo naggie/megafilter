7dd7196134beb027f4901ab77501f74ff80ccf7e
Callan Bryant <callan.bryant@gmail.com>
2013-04-10 22:46:41 +0200

    remove space key mapping

--- public/client.js
+++ public/client.js
@@ -17,7 +17,7 @@ mf.init = function() {
 	mf.nav.undo    = new mf.controllers.button('#undo')
 
 	mf.nav.skip.bind('right').bind('j').bind('s')
-	mf.nav.discard.bind('x').bind('d').bind('space')
+	mf.nav.discard.bind('x').bind('d')
 	mf.nav.publish.bind('p')
 	mf.nav.inspect.bind('i')
 	mf.nav.undo.bind('u').bind('ctrl+z')
